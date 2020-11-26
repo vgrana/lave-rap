53570f408ba8c873d7de16b5fb53707f3fd2ec32
vgrana <valeriagrandi@hotmail.com>
2020-11-25 21:30:40 -0300

    cambio de routes

--- src/index.js
+++ src/index.js
@@ -15,12 +15,37 @@ server.set('views',path.join(__dirname, 'views'));
 server.set('view engine', 'html');
 
 //routes
-server.use(require('./routes/index'));
-// server.get('/', (req,res) =>{
-//     res.sendFile(path.join(__dirname , '/views/index.html'));
+// server.use(require('./routes/index'));
+server.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname , '/views/index.html'));
     
-// });
-//
+});
+server.get('/img1', (req,res) =>{
+    res.sendFile(path.join(__dirname ,'/views/img1.jpg'));
+    
+});
+server.get('/img2', (req,res) =>{
+    res.sendFile(path.join(__dirname ,'/views/img2.jpg'));
+    
+});
+server.get('/img3', (req,res) =>{
+    res.sendFile(path.join(__dirname ,'/views/img3.jpg'));
+    
+});
+server.get('/img4', (req,res) =>{
+    res.sendFile(path.join(__dirname ,'/views/img4.jpg'));
+    
+});
+
+server.get('/face', (req,res) =>{
+    res.sendFile(path.join(__dirname ,'/views/face.jpg'));
+    
+});
+server.get('/instagram', (req,res) =>{
+    res.sendFile(path.join(__dirname ,'/views/instagram.jpg'));
+    
+});
+
 server.listen(server.get('port'), () => {
     console.log("Server running on port",server.get('port'));
   });
